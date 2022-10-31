ed9352658fafa842c61e3946521add5bb975ab27
Lsunae <sjsao5012@gmail.com>
2022-10-31 16:18:30 +0900

    MainActivity에 baseActivity 적용

--- app/src/main/java/com/leesunae/study_mvvm/view/MainActivity.kt
+++ app/src/main/java/com/leesunae/study_mvvm/view/MainActivity.kt
@@ -2,11 +2,12 @@ package com.leesunae.study_mvvm.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.leesunae.bebehelper_mvp.view.base.BaseActivity
 import com.leesunae.study_mvvm.R
+import com.leesunae.study_mvvm.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
