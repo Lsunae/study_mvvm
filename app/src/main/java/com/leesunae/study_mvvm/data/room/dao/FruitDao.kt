3ffe79d6b06203d3062d586967c1b11e6ba2a86b
Lsunae <sjsao5012@gmail.com>
2022-10-31 18:28:54 +0900

    과일 dao 구현

--- app/src/main/java/com/leesunae/study_mvvm/data/room/dao/FruitDao.kt
+++ app/src/main/java/com/leesunae/study_mvvm/data/room/dao/FruitDao.kt
@@ -1,4 +1,19 @@
 package com.leesunae.study_mvvm.data.room.dao
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.leesunae.study_mvvm.data.room.entity.FruitEntity
+
+@Dao
 interface FruitDao {
+
+    /** 과일 추가 */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertFruit(fruit: FruitEntity): Long
+
+    /** 과일 조회 */
+    @Query("SELECT * FROM fruit WHERE id = :id")
+    fun getFruit(id: Int): FruitEntity
 }
