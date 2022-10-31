c513aa9076b32810e14d2baa9e81652685aa4312
Lsunae <sjsao5012@gmail.com>
2022-10-31 15:50:06 +0900

    패키지 생성 및 메인 액티비티 view 패키지로 이동

--- app/src/main/java/com/leesunae/study_mvvm/view/MainActivity.kt
+++ app/src/main/java/com/leesunae/study_mvvm/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.leesunae.study_mvvm
+package com.leesunae.study_mvvm.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.leesunae.study_mvvm.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
