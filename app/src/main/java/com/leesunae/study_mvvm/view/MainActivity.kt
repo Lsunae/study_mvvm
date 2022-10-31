0428df1bd1a7c76fc08039e31f769e283249f295
Lsunae <sjsao5012@gmail.com>
2022-10-31 17:20:28 +0900

    viewModel 적용

--- app/src/main/java/com/leesunae/study_mvvm/view/MainActivity.kt
+++ app/src/main/java/com/leesunae/study_mvvm/view/MainActivity.kt
@@ -1,13 +1,33 @@
 package com.leesunae.study_mvvm.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.leesunae.bebehelper_mvp.view.base.BaseActivity
 import com.leesunae.study_mvvm.R
 import com.leesunae.study_mvvm.databinding.ActivityMainBinding
+import com.leesunae.study_mvvm.viewModel.FruitViewModel
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private val viewModel: FruitViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setupViewModel()
+        setClickEvent()
+    }
+
+    private fun setClickEvent() {
+        binding.apply {
+            tvBtn.setOnClickListener {
+                viewModel.setFruit(etInput.text.toString())
+            }
+        }
+    }
+
+    private fun setupViewModel() {
+        viewModel.fruit.observe(this) {
+            binding.inputFruit = it
+        }
     }
 }
