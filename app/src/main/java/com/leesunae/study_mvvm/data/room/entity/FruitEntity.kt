de32b3ac6a398045c3530f428effc00767a09e14
Lsunae <sjsao5012@gmail.com>
2022-10-31 18:26:32 +0900

    과일 entity 구현

--- app/src/main/java/com/leesunae/study_mvvm/data/room/entity/FruitEntity.kt
+++ app/src/main/java/com/leesunae/study_mvvm/data/room/entity/FruitEntity.kt
@@ -1,4 +1,14 @@
 package com.leesunae.study_mvvm.data.room.entity
 
-class FruitEntity {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "fruit")
+data class FruitEntity(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0,
+
+    @ColumnInfo(name = "fruitName")
+    var fruitName: String? = ""
+)
