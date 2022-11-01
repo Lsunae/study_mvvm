90e1ad219387173ddcd704c6edf98f436d09e757
Lsunae <sjsao5012@gmail.com>
2022-11-01 18:00:10 +0900

    주석 제거

--- app/src/main/java/com/leesunae/study_mvvm/data/room/dao/FruitDao.kt
+++ app/src/main/java/com/leesunae/study_mvvm/data/room/dao/FruitDao.kt
@@ -8,12 +8,9 @@ import com.leesunae.study_mvvm.data.room.entity.FruitEntity
 
 @Dao
 interface FruitDao {
-
-    /** 과일 추가 */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertFruit(fruit: FruitEntity): Long
 
-    /** 과일 조회 */
     @Query("SELECT * FROM fruit WHERE id = :id")
     fun getFruit(id: Int): FruitEntity
 }
