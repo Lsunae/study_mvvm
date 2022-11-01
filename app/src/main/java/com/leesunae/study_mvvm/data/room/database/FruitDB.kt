3754e1296ed44501b67dc166e493db95008093e8
Lsunae <sjsao5012@gmail.com>
2022-11-01 17:10:14 +0900

    DB 코드 추가

--- app/src/main/java/com/leesunae/study_mvvm/data/room/database/FruitDB.kt
+++ app/src/main/java/com/leesunae/study_mvvm/data/room/database/FruitDB.kt
@@ -1,4 +1,13 @@
 package com.leesunae.study_mvvm.data.room.database
 
-class FruitDB {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.leesunae.study_mvvm.data.room.dao.FruitDao
+import com.leesunae.study_mvvm.data.room.entity.FruitEntity
+
+@Database(entities = [FruitEntity::class], version = 1)
+abstract class FruitDB : RoomDatabase() {
+    abstract fun textDao(): FruitDao
+
+
 }
